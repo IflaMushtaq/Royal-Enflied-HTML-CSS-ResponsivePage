362afefc64cb2ce357e2496bdd39f0e962a737d0
Ifla Mushtaq <47410297+IflaMushtaq@users.noreply.github.com>
2023-01-24 15:05:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// For Toggle Menu
+function showMenu() {
+  document.getElementById("navLinks").style.right = "0";
+}
+function hideMenu() {
+  document.getElementById("navLinks").style.right = "-200px";
+}
